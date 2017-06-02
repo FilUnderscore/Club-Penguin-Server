155116425bd59020c09b43b0e6bf7540e938dd62
Filip <Filip@DESKTOP-VDTV9BC>
2017-06-02 20:49:48 +1200

    Log files now saved in /logs/yy-MM-dd HH-mm-ss.mmm.log

--- src/server/util/Logger.java
+++ src/server/util/Logger.java
@@ -1,12 +1,69 @@
 package server.util;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Paths;
+
 import server.Server;
 
 public class Logger 
 {
+	private static File File;
+	private static BufferedWriter FileWriter;
+	
+	static
+	{
+		try
+		{
+			String timestamp = Time.getTimestamp();
+			timestamp = timestamp.replaceAll(":", "-");
+			
+			File = Paths.get("logs/" + timestamp + ".log").toFile();
+			System.out.println(File.getAbsolutePath());
+			
+			if(!Paths.get("logs/").toFile().exists())
+			{
+				Paths.get("logs/").toFile().mkdirs();
+			}
+			
+			if(!File.exists())
+			{
+				File.createNewFile();
+			}
+			
+			FileWriter = new BufferedWriter(new FileWriter(File));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static String logToFile(String data)
+	{
+		try
+		{
+			if(data != null && data.length() > 0)
+			{
+				FileWriter.write(data);
+			}
+			
+			FileWriter.newLine();
+			
+			FileWriter.flush();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return data;
+	}
+	
 	public static void log(String info, LogType type, Server server)
 	{
-		System.out.println(Time.getTimestamp() + " [" + type.name() + "] " + "<" + (server != null && server.getServerInfo() != null ? server.getServerInfo().Name : "Server") + "> " + info);
+		System.out.println(logToFile(Time.getTimestamp() + " [" + type.name() + "] " + "<" + (server != null && server.getServerInfo() != null ? server.getServerInfo().Name : "Server") + "> " + info));
 	}
 	
 	public static void info(String msg, Server server)
