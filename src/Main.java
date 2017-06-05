17d5c87f1daa6c114cdc14f48bdb5d459c5c91c7
FilUnderscore <filunderscore@gmail.com>
2017-06-06 01:08:10 +1200

    Load EPF.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		CPServerAPI.getAPI().load(true, new File("filter.xml"));
+		CPServerAPI.getAPI().load(true, new File("filter.xml"), new File("epf.xml"));
 	}
 }
